4ec5d2007f56f201a64d5658e1e12b3a105148e0
Hung Nguyen <hungnguyen@Hungs-MacBook-Air.local>
2017-06-29 09:04:37 +0700

    show-hidden row

--- MoneyKeeper/MainTableVC.swift
+++ MoneyKeeper/MainTableVC.swift
@@ -15,6 +15,7 @@ class MainTableVC: UITableViewController {
         return storyboard.instantiateViewController(withIdentifier: "MainTableVC") as! MainTableVC
     }
     
+    @IBOutlet weak var borrowToPay: UIButton!
     @IBOutlet weak var textField: UITextField!
     
     @IBOutlet var rightView: UIView!
@@ -25,6 +26,57 @@ class MainTableVC: UITableViewController {
         self.textField.delegate = self
         self.textField.rightView = rightView
         self.textField.rightViewMode = .always
+        self.borrowToPay.isSelected = true
+    }
+    
+    enum CellIndex: Int {
+        case distance1
+        case textFieldCalcu
+        case distance2
+        case category
+        case description
+        case distance3
+        case account
+        case date
+        case payee
+        case event
+        case distance4
+        case borrow
+        case lender
+        case distance5
+        case addImage
+        case distance6
+        case save
+        case distance7
+    }
+    
+    @IBAction func borrowToPay(_ sender: Any) {
+        if borrowToPay.isSelected{
+            borrowToPay.setImage(UIImage(named: "Checked"), for: .normal)
+            borrowToPay.isSelected = false
+        } else {
+            borrowToPay.setImage(UIImage(named: "Unchecked"), for: .normal)
+            borrowToPay.isSelected = true
+        }
+        tableView.reloadData()
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case CellIndex.lender.rawValue:
+            return borrowToPay.isSelected ? 0 : 46
+        case CellIndex.textFieldCalcu.rawValue:
+            return 70
+        case CellIndex.distance1.rawValue, CellIndex.distance7.rawValue:
+            return 10
+        case CellIndex.distance2.rawValue,
+             CellIndex.distance3.rawValue,
+             CellIndex.distance4.rawValue,
+             CellIndex.distance5.rawValue,
+             CellIndex.distance6.rawValue:
+            return 16
+        default:
+            return 46
+        }
     }
 }
 extension MainTableVC: UITextFieldDelegate{
